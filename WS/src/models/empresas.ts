c92660643295e88cb2d918a10d7228f0de3d17b9
Guilherme <guilhermemcl1503@gmail.com>
2025-04-18 07:41:11 -0300

    Feat : Criando Model de Empresas

---
+++ WS/src/models/empresas.ts
@@ -0,0 +1,49 @@
+
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const empresa = new Schema({
+    nome: {
+        type: String,
+        required: [true, 'Nome é obrigatório'],
+    },
+    foto: String,
+    capa: String,
+    email: {
+        type: String,
+        required: [true, 'Nome é obrigatório'],
+    },
+    senha: String,
+    telefone: String,
+    endereco: {
+        cidade: String,
+        uf: String,
+        bairro: String,
+        rua: String,
+        numero: String,
+        cep: String,
+        pais: String
+    },
+    geo: {
+        tipo: String,
+        cordinates: Array
+
+    },
+    dataCadastro: {
+        type: Date,
+        default: Date.now
+    }
+
+});
+
+
+
+
+
+
+empresa.index({ geo: '2dsphere' });
+
+
+
+module.exports = mongoose.model('Empresa', empresa);
